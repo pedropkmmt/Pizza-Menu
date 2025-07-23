9e3e2c5c359766187ca8a4f82864a552a1cdcf56
Pedro <pedropkmmt@gmail.com>
2025-07-23 22:13:21 +0200

    code updates on footer and app

--- Pizza-Fast/pizza-fast/src/App.jsx
+++ Pizza-Fast/pizza-fast/src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css'
 import Pizza from './Components/Pizzas'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 function App(){
     return(
         <>
         <div className='container'>
         <Header/>
         <Pizza/>
+        <Footer/>
         </div>
         </>
     )
--- Pizza-Fast/pizza-fast/src/Components/Footer.jsx
+++ Pizza-Fast/pizza-fast/src/Components/Footer.jsx
@@ -1,6 +1,20 @@
 const Footer = () => {
+    const hour = new Date().getHours();
+    const openHour = 9
+    const closedHour = 18
+    // if (hour >= openHour && hour <= closedHour){
+    //     alert("We are open")
+    // }
+    // else{
+    //     alert("We are closed")
+    // }
+  
     return(
-        <></>
+        <>
+        <footer className="footer">
+            {new Date().toLocaleDateString()}" We're currently open!"
+        </footer>
+        </>
     )
 }
 export default Footer
