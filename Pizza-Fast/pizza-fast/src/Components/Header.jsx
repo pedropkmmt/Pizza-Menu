f1975a4081f37f62d4b548a27fcad2403650bcdd
Pedro <pedropkmmt@gmail.com>
2025-07-25 14:43:39 +0200

    updated designs

--- Pizza-Fast/pizza-fast/src/Components/Header.jsx
+++ Pizza-Fast/pizza-fast/src/Components/Header.jsx
@@ -1,13 +1,17 @@
 const Header = () => {
     return (
-        <>
+      <>
         <header className="header">
-            <h1>Fast React Pizza</h1>
+          <h1>Fast Italian Pizza</h1>
         </header>
         <div className="menu">
-            <h2>Our Menu</h2>
+          <h2>Our Menu</h2>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our store oven, all organic , all delicious.
+          </p>
         </div>
-        </>
-    )
+      </>
+    );
 }
 export default Header
--- Pizza-Fast/pizza-fast/src/Components/Pizzas.jsx
+++ Pizza-Fast/pizza-fast/src/Components/Pizzas.jsx
@@ -1,18 +1,26 @@
 import pizzaData from '../data'
 function Pizza(){
-    return(
-        <>
-    {pizzaData.map((pizza, key) => (
+    return (
+      <>
+        {pizzaData.map((pizza) => (
+          <li className="pizza" key={pizza.name}>
             <div className='pizza'>
-                {key = pizza.name}
-            <img src={pizza.photoName} alt={pizza.photoName} />
-            <h2>{pizza.name}</h2>
-            <span>R{pizza.price}</span>
-            <p>{pizza.ingredients}</p>
+              {pizza.soldOut ? (
+                <div className="sold-out">
+                  <img src={pizza.photoName} alt={pizza.photoName} />
+                </div>
+              ) : (
+                <img src={pizza.photoName} alt={pizza.photoName} />
+              )}
             </div>
-            ))}
-        
-        </>
-    )
+            <div>
+              <h2>{pizza.name}</h2>
+              <p>{pizza.ingredients}</p>
+              <span>{pizza.soldOut ? "Sold Out" : `R ${pizza.price}`}</span>
+            </div>
+          </li>
+        ))}
+      </>
+    );
 }
 export default Pizza
